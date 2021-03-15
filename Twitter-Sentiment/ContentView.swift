cd6f4da607a306099b05d15cfb36f81117fdae94
Jess <jtrinh619@gmail.com>
2021-03-15 12:29:05 -0700

    [tested] performance of sentiment classification

--- Twitter-Sentiment/ContentView.swift
+++ Twitter-Sentiment/ContentView.swift
@@ -7,13 +7,16 @@
 
 import SwiftUI
 import SwifteriOS
+import CoreML
 
 struct ContentView: View {
     @State var searchTerm: String = ""
-    @State private var searchButtonPressed = false
     @State var sentiment = "😁"
     
+    // authenticate w/ Twitter API through Swifter framework
     let swifter = Swifter(consumerKey: apiKey, consumerSecret: apiSecretKey)
+    
+    let sentimentClassifier = TweetSentimentClassifier()
     
     
     var body: some View {
@@ -37,22 +40,37 @@ struct ContentView: View {
                 }
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 
-                Button(
-                    action: searchTweet,
-                    label: {Text("Search")}
-                )
+                Button(action: {
+                    searchTweet()
+                    testPrediction()
+                }) {
+                    Text("Search")
+                }
             }.padding()
         }
     }
     
+    // filter tweets, performing search query,
+    // in english w/ maximum of 100 tweets returned
+    // and .extended to return full/max 280 characters from Twitter
     func searchTweet() {
-        swifter.searchTweet(using: "@Apple") { (results, metadata) in
-            print(results)
-        } failure: { (error) in
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
+//            print(results)
+        }) { (error) in
             print("Error w/ API Request: \(error)")
         }
+    }
+    
+    func testPrediction() {
+        let negPrediction = try! sentimentClassifier.prediction(text: "@Apple is the worst!")
+        let posPrediction = try! sentimentClassifier.prediction(text: "@Apple is the best!")
+        
+        print(negPrediction.label)
+        print(posPrediction.label)
 
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
